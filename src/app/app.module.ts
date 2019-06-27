a4289086faf3d3428be5350ad9076cb0786a70ff
Kyrune <43679939+Kyrune@users.noreply.github.com>
2019-06-27 16:19:03 -0500

    Added nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { SkillsComponent } from './skills/skills.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ProjectsComponent,
-    SkillsComponent
+    SkillsComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
