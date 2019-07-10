4d66bef7a8cae3b2297e712c063284103922b154
Kyrune <43679939+Kyrune@users.noreply.github.com>
2019-07-10 16:46:59 -0500

    Added css, html, and typescript

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  city = 'Where I live: Cashton, Wisconsin';
+  aboutMe = `I\'m a junior full stack developer and I enjoy creating stuff! I pride myself on a sense of humor
+  and the ability to sit in front of a screen for hours on end in blissful solitude. I love learning new things,
+  honing my coding skills, and designing websites for optimal user experience, creativity, and efficiency.`;
+
   constructor() { }
 
   ngOnInit() {
