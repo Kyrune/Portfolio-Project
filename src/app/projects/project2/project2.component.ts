b33e855ddb93fdc25f7391a9dd8bf4a82a4dfa50
Kyrune <43679939+Kyrune@users.noreply.github.com>
2019-08-22 15:51:58 -0500

    Add project description

--- src/app/projects/project2/project2.component.ts
+++ src/app/projects/project2/project2.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project2.component.css']
 })
 export class Project2Component implements OnInit {
+  ionic = `An Android application that allows users to turn pages and navigate
+  to any chapter from the Table of Contents (TOC), as well as navigate back to
+  the TOC from any page. This project includes an experimental sample from one of my stories.`;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
