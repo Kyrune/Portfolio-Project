016c9079e4d387a9400ae68c6d0ba97b7ffc219b
Kyrune <43679939+Kyrune@users.noreply.github.com>
2019-09-02 11:44:30 -0500

    Edit ionic description & remove ngOnInit

--- src/app/projects/project2/project2.component.ts
+++ src/app/projects/project2/project2.component.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-project2',
   templateUrl: './project2.component.html',
   styleUrls: ['./project2.component.css']
 })
-export class Project2Component implements OnInit {
+export class Project2Component {
   ionic = `An Android application that allows users to turn pages and navigate
   to any chapter from the Table of Contents (TOC), as well as navigate back to
-  the TOC from any page. This project includes an experimental sample from one of my stories.`;
+  the TOC from any page. This project includes an experimental sample from one of my planned stories.
+  The front page image was created by me, using Photoshop and a Wacom tablet.`;
 
   constructor() {}
-
-  ngOnInit() {}
 }
