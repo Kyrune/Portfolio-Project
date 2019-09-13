fa75646fdf462e7ffaa01b01014b12ca3d6eb28d
Kyrune <43679939+Kyrune@users.noreply.github.com>
2019-09-13 11:56:50 -0500

    Organize project3 component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { Project1Component } from './projects/project1/project1.component';
 import { SkillsComponent } from './skills/skills.component';
 import { NavComponent } from './nav/nav.component';
 import { SkillsService } from './skills/skills.service';
 import { AboutComponent } from './about/about.component';
-import { Project2Component } from './projects/project2/project2.component';
+import { Project1Component } from './projects/project1/project1.component';
 import { ProjectsPageComponent } from './projects/projects-page/projects-page.component';
+import { Project2Component } from './projects/project2/project2.component';
+import { Project3Component } from './projects/project3/project3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    Project1Component,
     SkillsComponent,
     NavComponent,
     AboutComponent,
+    ProjectsPageComponent,
+    Project1Component,
     Project2Component,
-    ProjectsPageComponent
+    Project3Component
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [SkillsService],
