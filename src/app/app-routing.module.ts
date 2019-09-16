bdcf33b6e1d7d81e008a6ca5273761f2d94588eb
Kyrune <43679939+Kyrune@users.noreply.github.com>
2019-09-16 17:16:17 -0500

    Reorganize files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { SkillsComponent } from './skills/skills.component';
-import { ProjectsPageComponent } from './projects/projects-page/projects-page.component';
-import { Project1Component } from './projects/project1/project1.component';
-import { Project2Component } from './projects/project2/project2.component';
-import { Project3Component } from './projects/project3/project3.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsPageComponent } from './components/projects/projects-page/projects-page.component';
+import { Project1Component } from './components/projects/project1/project1.component';
+import { Project2Component } from './components/projects/project2/project2.component';
+import { Project3Component } from './components/projects/project3/project3.component';
 
 const routes: Routes = [
   {
