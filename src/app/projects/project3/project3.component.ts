a069f9f6faf387129f65d0e5912142d6b857ed5d
Kyrune <43679939+Kyrune@users.noreply.github.com>
2019-09-16 15:28:41 -0500

    Add descriptions for project3

--- src/app/projects/project3/project3.component.ts
+++ src/app/projects/project3/project3.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project3.component.css']
 })
 export class Project3Component implements OnInit {
+  portfolio = `This is my personal Portfolio website, which you are currently perusing.
+  It was built from scratch using the Angular framework, and you are welcome to look
+  at the code used in its construction (GitHub link below).
+  I will continue to work on it as my coding skills grow,
+  and plan to eventually shift it to a different framework/library.`;
 
-  constructor() { }
+  royal = `My cyborg logo, derived from the team logo I made for Project MindStorm,
+  was made even cooler by Cameron Schilling, the lead Graphic Designer at
+  Royal Branding. Check out his website below!`;
 
-  ngOnInit() {
-  }
+  constructor() {}
 
+  ngOnInit() {}
 }
