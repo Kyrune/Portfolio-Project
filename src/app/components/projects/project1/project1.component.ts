d2bd03f3885c68fbc810d2be753afbde5b2543c5
Kyrune <43679939+Kyrune@users.noreply.github.com>
2019-09-17 10:14:38 -0500

    Add list of tech

--- src/app/components/projects/project1/project1.component.ts
+++ src/app/components/projects/project1/project1.component.ts
@@ -6,9 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project1.component.css']
 })
 export class Project1Component implements OnInit {
+  /* Project description */
   mindstorm = `Group project MindStorm is a MEAN stack website geared toward writers
   for the purpose of providing a place to brainstorm ideas for stories
   and characters, while displaying stories for constructive criticism.`;
+
+  /* List of tech used in Project */
+  tech = `HTML5, CSS, Angular, TypeScript (TS), Angular Materials, MongoDB,
+  Mongoose, JWT, Multer, mLab, Express, Node, NPM, REST, Compodoc, AWS,
+  Visual Studio Code, Git and GitHub`;
 
   constructor() {}
 
