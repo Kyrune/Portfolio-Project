ec6f861ed30d9f5ce9d147b971065abd86983cd9
Kyrune <43679939+Kyrune@users.noreply.github.com>
2019-09-18 13:00:43 -0500

    Add comments to nav component

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+/** Navigation component */
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
+  /** Author's name */
   title = 'Maria Ziemer';
 }
