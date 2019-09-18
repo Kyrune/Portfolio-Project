7c1bf50e2978581bd064ecc5694ab903fcfb1bb4
Kyrune <43679939+Kyrune@users.noreply.github.com>
2019-09-18 12:59:51 -0500

    Add comments to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+/** App component */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  /** Project name */
   title = 'portfolio-project';
 }
