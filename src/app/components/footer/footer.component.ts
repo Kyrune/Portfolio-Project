245b4b43261da903d55c4f189ee9303e423cf59b
Kyrune <43679939+Kyrune@users.noreply.github.com>
2019-09-18 13:00:13 -0500

    Add comments to footer component

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+/** Footer component */
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
