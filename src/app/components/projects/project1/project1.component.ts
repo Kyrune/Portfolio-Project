9b734eeab07c13afd39375fc58148d28d8bff395
Kyrune <43679939+Kyrune@users.noreply.github.com>
2019-09-18 13:00:57 -0500

    Add comments to project1 component

--- src/app/components/projects/project1/project1.component.ts
+++ src/app/components/projects/project1/project1.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+/** 1st project component */
 @Component({
   selector: 'app-project1',
   templateUrl: './project1.component.html',
   styleUrls: ['./project1.component.css']
 })
 export class Project1Component implements OnInit {
-  /* Project description */
+  /** Project description */
   mindstorm = `Group project MindStorm is a MEAN stack website geared toward writers
   for the purpose of providing a place to brainstorm ideas for stories
   and characters, while displaying stories for constructive criticism.`;
 
-  /* List of tech used in Project */
+  /** List of tech used in Project MindStorm */
   tech = `HTML5, CSS, Angular, TypeScript (TS), Angular Materials, MongoDB,
   Mongoose, JWT, Multer, mLab, Express, Node, NPM, REST, Compodoc, AWS,
   Visual Studio Code, Git and GitHub`;
