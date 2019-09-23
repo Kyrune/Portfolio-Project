b64d5f82f3ca9e33967cd5fb4892ac37dbe7e0a1
Kyrune <43679939+Kyrune@users.noreply.github.com>
2019-09-23 15:28:49 -0500

    Remove unneeded OnInit & constructor

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 /** Home component */
 @Component({
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   /** Author's description */
   aboutMe = `I\'m a junior web developer and I enjoy creating stuff!
   My interests lie in frontend development and web design, where I can combine
@@ -15,8 +15,4 @@ export class HomeComponent implements OnInit {
   as attested to by the blinding white bloom that is my face.
   I love learning new things, and aim to learn from those more experienced than myself as I pursue
   opportunities to bring creativity and passion to my code, team and work environment.`;
-
-  constructor() {}
-
-  ngOnInit() {}
 }
