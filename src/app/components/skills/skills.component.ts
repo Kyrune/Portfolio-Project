b977d9b49289e0a999c71f2e4246023c9ac57840
Kyrune <43679939+Kyrune@users.noreply.github.com>
2019-09-23 15:28:15 -0500

    Remove unneeded OnInit

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { Skills } from '../../shared/skills.model';
 import { SkillsService } from '../../shared/skills.service';
@@ -9,7 +9,7 @@ import { SkillsService } from '../../shared/skills.service';
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.css']
 })
-export class SkillsComponent implements OnInit {
+export class SkillsComponent {
   /** Skills array in Skills Service */
   skills: Skills[];
 
@@ -17,6 +17,4 @@ export class SkillsComponent implements OnInit {
   constructor(private skillsService: SkillsService) {
     this.skills = skillsService.skills;
   }
-
-  ngOnInit() {}
 }
