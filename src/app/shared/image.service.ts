5bfa8973145a747af49113f518e2598b4d55e81d
Kyrune <43679939+Kyrune@users.noreply.github.com>
2019-11-11 12:00:17 -0600

    Add imageGallery functions/data

--- src/app/shared/image.service.ts
+++ src/app/shared/image.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
 
+const Imagesdetails = [
+  { id: 1, name: 'The Main Page', url: 'assets/images/1-mainpage' }
+];
+
 @Injectable({
   providedIn: 'root'
 })
 export class ImageService {
+  imageGallery = [];
 
-  constructor() { }
+  constructor() {}
+
+  getImages() {
+    return (this.imageGallery = Imagesdetails.slice(0));
+  }
+
+  getImage(id: number) {
+    return Imagesdetails.slice(0).find(Images => Images.id == id);
+  }
 }
