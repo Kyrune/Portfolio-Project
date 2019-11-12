a452f2fe976452098e891bc225e4d527eba657c9
Kyrune <43679939+Kyrune@users.noreply.github.com>
2019-11-12 13:13:27 -0600

    Add ngx-gallery options/images

--- src/app/components/projects/project1/project1.component.ts
+++ src/app/components/projects/project1/project1.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  NgxGalleryOptions,
+  NgxGalleryImage,
+  NgxGalleryAnimation
+} from 'ngx-gallery';
 
 /** 1st project component */
 @Component({
@@ -6,7 +11,10 @@ import { Component } from '@angular/core';
   templateUrl: './project1.component.html',
   styleUrls: ['./project1.component.css']
 })
-export class Project1Component {
+export class Project1Component implements OnInit {
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
+
   /** Project description */
   mindstorm = `Group project MindStorm is a MEAN stack website geared toward writers
   for the purpose of providing a place to brainstorm ideas for stories
@@ -16,4 +24,45 @@ export class Project1Component {
   tech = `HTML5, CSS, Angular, TypeScript (TS), Angular Materials, MongoDB,
   Mongoose, JWT, Multer, mLab, Express, Node, NPM, REST, Compodoc, AWS,
   Visual Studio Code, Git and GitHub`;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.galleryOptions = [
+      {
+        width: '700px',
+        height: '400px',
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide
+      },
+      /** Max-width 800 */
+      {
+        breakpoint: 800,
+        width: '100%',
+        height: '600px',
+        imagePercent: 80,
+        thumbnailsPercent: 20,
+        thumbnailsMargin: 20,
+        thumbnailMargin: 20
+      },
+      /** Max-width 400 */
+      {
+        breakpoint: 400,
+        preview: false
+      }
+    ];
+
+    this.galleryImages = [
+      {
+        small: '../assets/images/1-mainpage.jpg',
+        medium: '../assets/images/1-mainpage.jpg',
+        big: '../assets/images/1-mainpage.jpg'
+      },
+      {
+        small: '../assets/images/2-mainpage.jpg',
+        medium: '../assets/images/2-mainpage.jpg',
+        big: '../assets/images/2-mainpage.jpg'
+      }
+    ];
+  }
 }
